21961d2e2882458ad6a2b6810fca91412dcc90f2
Michiel <918128+mvanderlee@users.noreply.github.com>
2025-09-11 00:32:53 +0200

    Change name to try and release via my pypi

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from pathlib import Path
-from setuptools import setup, Extension
+
 import Cython.Build
+from setuptools import Extension, setup
 
 # read the contents of your README file
 this_directory = Path(__file__).parent
@@ -11,7 +12,7 @@ version = '0.0.6'
 ext = Extension(name="UltraDict", sources=["UltraDict.py"])
 
 setup(
-    name='UltraDict',
+    name='UltraDict-wheels',
     version=version,
     description='Sychronized, streaming dictionary that uses shared memory as a backend',
     long_description=long_description,
@@ -25,5 +26,5 @@ setup(
     zip_safe=False,
     ext_modules=Cython.Build.cythonize(ext, compiler_directives={'language_level' : "3"}),
     setup_requires=['cython>=0.24.1'],
-    python_requires=">=3.8",
+    python_requires=">=3.9",
 )
